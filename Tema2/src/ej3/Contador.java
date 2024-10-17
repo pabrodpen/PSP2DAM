f05dabbbf121636c94753594eabf77a443104859
pabrodpen <pabrodpen@gmail.com>
2024-10-17 14:35:58 +0200

    add

---
+++ Tema2/src/ej3/Contador.java
@@ -0,0 +1,12 @@
+package ej3;
+
+import java.applet.Applet;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Contador extends Applet implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
